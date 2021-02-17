9561036602bc2dec33f23593cd19ac23a59bc3d4
Dhairya1734 <dhairyamaniyar2020@gmail.com>
2021-02-17 16:43:36 +0530

    implemented Navigation as per comments

--- reactproject/src/Navigation.js
+++ reactproject/src/Navigation.js
@@ -5,24 +5,43 @@ import PropTypes from 'prop-types'
 
 const Navigation = React.memo((props) => {
 
-    const [shouldDisplayPrevious, setShouldDisplayPrevious] = useState(0);
-    const [shouldDisplayItemList, setShouldDisplayItemList] = useState(0);
+    // const [shouldDisplayPrevious, setShouldDisplayPrevious] = useState(0);
+    // const [shouldDisplayItemList, setShouldDisplayItemList] = useState(0);
+
+    // const displayItemList = useCallback(() => {
+    //     setShouldDisplayItemList((shouldDisplayItemList + 1) % 2);
+    // }, [shouldDisplayItemList]);
+
+    // const displayPreviousList = useCallback(() => {
+    //     setShouldDisplayPrevious((shouldDisplayPrevious + 1) % 2);
+    // }, [shouldDisplayPrevious]);
+
+    const [visibleBlock , setVisibleBlock] = useState(null);
 
     const displayItemList = useCallback(() => {
-        setShouldDisplayItemList((shouldDisplayItemList + 1) % 2);
-    }, [shouldDisplayItemList]);
+        if(visibleBlock != "itemNavigation"){
+            setVisibleBlock("itemNavigation");
+        }
+        else if( visibleBlock == "itemNavigation"){
+            setVisibleBlock(null);
+        }
+    }, [visibleBlock]);
 
     const displayPreviousList = useCallback(() => {
-        setShouldDisplayPrevious((shouldDisplayPrevious + 1) % 2);
-    }, [shouldDisplayPrevious]);
-
+        if(visibleBlock != "PreviousOrder"){
+            setVisibleBlock("PreviousOrder");
+        }
+        else if( visibleBlock == "PreviousOrder"){
+            setVisibleBlock(null);
+        }
+    }, [visibleBlock]);
 
     return (
         <nav className="boxType" id="navigation">
             <Suspense fallback = {<div className="loading"> This is Loading </div>}>
                 <ol id="displayItems">
                     <li><button className="white mainNav" onClick={displayItemList}><strong>Items</strong> </button><br />
-                        {shouldDisplayItemList === 1 ? <ol className="navItemName">
+                        {visibleBlock === "itemNavigation" ? <ol className="navItemName">
                             {Object.keys(props.itemHeading).map(key => {
                                 return <DisplayNavigationItemHeading key={key} id={key} name={props.itemHeading[key].visibleName} />
                             })}
@@ -32,7 +51,7 @@ const Navigation = React.memo((props) => {
                         <button className="white mainNav" onClick={displayPreviousList}> <strong>My Order</strong></button><br />
                     </li>
                 </ol>
-                { shouldDisplayPrevious === 1 ?<DisplayPreviousOrder itemList={props.itemList} displayPreviousListHandler={displayPreviousList} />: null}
+                { visibleBlock == "PreviousOrder" ?<DisplayPreviousOrder itemList={props.itemList} displayPreviousListHandler={displayPreviousList} />: null}
             </Suspense>
         </nav>
     );
